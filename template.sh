9410de1644a89ec75d71965dc6c4c17fadcb2c95
Pallab Sing <pallabsing@Pallabs-MacBook-Pro.local>
2025-12-05 09:12:42 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creat directory
+mkdir -p src
+mkdir -p resarch
+mkdir -p resarch
+
+#creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch resarch/trials.ipynb
+touch requirement.txt
+
+echo "directory and file created sucessfully"
